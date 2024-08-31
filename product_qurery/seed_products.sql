6e524463d145593e54b48af4d9b09a680266fbc5
MartinOrji <martin.orji.jnr@gmail.com>
2024-08-31 09:47:25 -0500

    first commit

---
+++ product_qurery/seed_products.sql
@@ -0,0 +1,17 @@
+
+-- in terminal:
+-- psql < seed_products.sql
+-- psql products_db
+
+DROP DATABASE IF EXISTS  products_db;
+
+CREATE DATABASE products_db;
+
+\c products_db
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  price FLOAT CHECK (price > 0),
+  can_be_returned BOOLEAN NOT NULL
+);
